a22981bf8bd04fb3486ab6b68c6fe495daa16f1a
BillMatera <matera.3@osu.edu>
2017-04-05 20:13:38 -0400

    ok

---
+++ FisherLink/FisherLink/Data/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+using FisherLink.Models;
+
+
+namespace FisherLink.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
+
+    }
+}
